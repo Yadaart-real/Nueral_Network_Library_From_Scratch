3ad18ac26c7867493acf2ac758b9c9ca0d4bd26d
Yatharth Keshavamurthy <yatharthkeshavamurthy@gmail.com>
2026-07-17 01:56:06 +0530

    Optimize neural network with He initialization and ReLU
    Implemented He initialization for weights to improve ReLU activation performance and prevent dead neurons. Updated activation functions to include ReLU and its derivative, and modified training logic accordingly.

--- YKMNueralNet.py
+++ YKMNueralNet.py
@@ -1,9 +1,13 @@
-# Next steps : upgrade this architecture to support multiple hidden layers (check IRL papers for plan)
+# Next steps : upgrade this architecture to support multiple hidden layers (check IRL papers for plan) : DONE
+# Optimization and betterment : right now code works for relu activaiton because relu function derivative gives zero even for a negative value so it doesn't matter if we pass in pre activation values or post activation values of matrix since either will give same output as again relu derviative gives 0 for negative values and 0 for 0 aswell
+# In the future change the gradient descent code to include my new calculation having derivative taken of pre activation layer to allow for more better activation functions
+# another issue ive run into is DEADRLU's so during original weight initialization im hard coding a standard deviation of 0.1 and normal distribution of 0, almost 50% of nuerons are deadRLU's which means they get negative values and remain zero thus never changing and thus not affecting the weights during training
+# solution to this issue: He initalization of weights ( a design to keep ReLU's alive) -> scales the weights based on the number of incoming connections  basically if we take the weight between Input layer and first hidden layer, we get the number of incoming connections as the no. of nuerons of input layer, since each nueron connects to the hidden layers nueron atleast once
+# HE INITIALIZATION -> Specifically for RELu breaks for other activations
 
 import numpy as np
+from streamlit import delta_generator
 
-
-#import math
 
 class YathNeuralNet:
     rng = np.random.default_rng()
@@ -19,49 +23,63 @@ class YathNeuralNet:
         self.allErrors : list[np.ndarray | None] = [None] * (len(self.hidden_nueron_array) + 1)
         #initializing learning rate
         self.learning_rate = learning_rate
-        # initialzing temporary weight matrices and weight gradients array and biases and biases gradient
+        # initialzing temporary weight matrices and biases matrices arrays
         self.allweightsarray : list[np.ndarray | None] = [None] * (len(self.hidden_nueron_array) + 1)
-        self.allweightgradeintsarray : list[np.ndarray | None] = [None] * (len(self.allweightsarray))
+
         self.biases_array : list[np.ndarray | None] = [None] * (len(self.allweightsarray))
-        self.biases_gradients_array : list[np.ndarray | None] = [None] * (len(self.allweightsarray))
+
 
         #weight and biases initialization loop -> loops over only length initialized all weights array
+        #recent HE initalization implementation
         for i in range(0, len(self.allweightsarray)):
-            if i == 0: # for first weight btw Input layer and Foremost Hidden Layer
-                self.allweightsarray[i] = self.rng.normal(0, 1.0, (self.hidden_nueron_array[i], self.input_nuerons))
-                self.biases_array[i] = self.rng.normal(-1.0, 1.0, (self.hidden_nueron_array[i], 1))
-            elif i == len(self.allweightsarray) - 1: # for last weight btw Outermost Hidden Layer and Output layer
-                self.allweightsarray[i] = self.rng.normal(0, 1.0, (self.output_nuerons, self.hidden_nueron_array[i - 1]))
-                self.biases_array[i] = self.rng.normal(-1.0, 1.0, (self.output_nuerons, 1))
-            else: # for every other weight btw each hidden layer
-                self.allweightsarray[i] = self.rng.normal(0, 1.0, (self.hidden_nueron_array[i], self.hidden_nueron_array[i-1]))
-                self.biases_array[i] = self.rng.normal(-1.0, 1.0, (self.hidden_nueron_array[i], 1))
+            # recently implementing He initi std dev = root 2/ n(incoming)
 
-    def sigmoid(self,x):  # the sigmoid function for activation
+            if i == 0: # for first weight btw Input layer and Foremost Hidden Layer
+                std_dev = np.sqrt(2.0/ self.input_nuerons)
+                self.allweightsarray[i] = self.rng.normal(0, std_dev, (self.hidden_nueron_array[i], self.input_nuerons))
+                self.biases_array[i] = np.ones((self.hidden_nueron_array[i], 1)) * 0.01
+            elif i == len(self.allweightsarray) - 1: # for last weight btw Outermost Hidden Layer and Output layer
+                std_dev = np.sqrt(1.0 / self.hidden_nueron_array[-1])
+                self.allweightsarray[i] = self.rng.normal(0, std_dev, (self.output_nuerons, self.hidden_nueron_array[-1]))
+                self.biases_array[i] = np.zeros((self.output_nuerons, 1))
+            else: # for every other weight btw each hidden layer
+                std_dev = np.sqrt(2.0 / self.hidden_nueron_array[i-1])
+                self.allweightsarray[i] = self.rng.normal(0, std_dev, (self.hidden_nueron_array[i], self.hidden_nueron_array[i-1]))
+                self.biases_array[i] = np.ones((self.hidden_nueron_array[i], 1)) * 0.01
+
+    def sigmoid(self,x):  # the sigmoid function for activation of output layers
         return 1 / (1 + np.exp(-x))  # clean probablity between 0 and 1
 
     def ultasigmoid(self, x): # ultasigmoid or the derivative of the sigmoid function
         return x * (1 - x)
 
+    def relu_act(self, x): # the relu function for activation of hidden layers, processes the values to be only positive
+        return np.maximum(0, x)
+
+    def ulta_relu_act(self, x): # the derivative of the relu function at x = 0 is undefined but we keep it 0
+        return np.where(x <= 0, 0, 1)  # returns 1 if x > 0 and 0 if x < or = to 0
+
+
+    # Feed Forward Algorithm
     def feedforward(self, input_array):
         input_matrix = np.array(input_array).reshape(-1, 1) # this makes the list into a matrix and converts it from 1x2 to 2x1 for our matrix math
         # loop to calculate values of each elemement in every nueron of hidden matrices in every layer
         for x in range(0, len(self.hidden_matrix_PL_array)):
             if x == 0: # for foremost Hidden Layer (with Input)
-                self.hidden_matrix_PL_array[x] = self.sigmoid(np.dot(self.allweightsarray[x], input_matrix) + self.biases_array[x])
+                self.hidden_matrix_PL_array[x] = self.relu_act(np.dot(self.allweightsarray[x], input_matrix) + self.biases_array[x])
             else: # for every other hidden layer, only goes until the last index in no of layers
-                self.hidden_matrix_PL_array[x] = self.sigmoid(np.dot(self.allweightsarray[x], self.hidden_matrix_PL_array[x - 1]) + self.biases_array[x])
+                self.hidden_matrix_PL_array[x] = self.relu_act(np.dot(self.allweightsarray[x], self.hidden_matrix_PL_array[x - 1]) + self.biases_array[x])
         # calculates the output matrix result
         output_matrix = self.sigmoid(np.dot(self.allweightsarray[-1], self.hidden_matrix_PL_array[-1]) + self.biases_array[-1])
         # converts into readable output
         converted_list = output_matrix.tolist()
         # rounding all values
-        for i in range(0, len(converted_list)):
-            converted_list[i] = round(converted_list[i][0])
+        #for i in range(0, len(converted_list)):
+            #converted_list[i] = round(converted_list[i][0])
 
         return converted_list
 
-    # Backpropogation : transpose the weight matrix HO, mulitply scalar Error (previous)
+
     # weight gradient = (lr * Error_mat * (act_g(1 - act_g))) x Transpose_layer (O -> H, H -> I)
     def train(self, input_array, target_array):
         #converting user given arrays to matrices for further calculation
@@ -71,47 +89,37 @@ class YathNeuralNet:
         #hidden calculation - > caculating hidden matrices per layer
         for x in range(0, len(self.hidden_matrix_PL_array)):
             if x == 0:  # for foremost Hidden Layer (with Input)
-                self.hidden_matrix_PL_array[x] = self.sigmoid(np.dot(self.allweightsarray[x], input_matrix) + self.biases_array[x])
+                self.hidden_matrix_PL_array[x] = self.relu_act(np.dot(self.allweightsarray[x], input_matrix) + self.biases_array[x])
             else: # for every other hidden layer
-                self.hidden_matrix_PL_array[x] = self.sigmoid(np.dot(self.allweightsarray[x], self.hidden_matrix_PL_array[x - 1]) + self.biases_array[x])
+                self.hidden_matrix_PL_array[x] = self.relu_act(np.dot(self.allweightsarray[x], self.hidden_matrix_PL_array[x - 1]) + self.biases_array[x])
 
         #output_calculation - > calculating the final output matrix layer
         output_matrix = self.sigmoid(np.dot(self.allweightsarray[-1], self.hidden_matrix_PL_array[-1]) + self.biases_array[-1])## calculating output matrix
 
-
+        # Backpropogation : transpose the weight matrix HO
         #Backpropogation loop -> stores all errors for each hidden layer including output layer in dedicated errors array
         for y in range(len(self.allErrors)-1, -1, -1): # loops backwards
             if y == len(self.allErrors)-1: # for output matrix error
                 self.allErrors[y] = target_matrix - output_matrix
-            else: # for all other hidden layers
+                #self.allErrors[y] = output_matrix - target_matrix
+
+            else:
                 self.allErrors[y] = np.dot(np.transpose(self.allweightsarray[y+1]), self.allErrors[y+1])
 
 
-        # Gradient Calculation
+        # Gradient Calculation, # weight gradient = (lr * Error_mat(ahead layer) * derivative w.r.t act function of ahead layer x Transpose_layer (O -> H, H -> I)
         # Calculating Weight gradient and biases gradient for weights between Hidden and Output layers and biases of each hidden layer and output layer
-        #for z in range(0, len(self.allweightgradeintsarray)):
-            #if z == 0:
-                #self.allweightgradeintsarray[z] = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z])), np.transpose(input_matrix))
-                #self.biases_gradients_array[z] = np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z]))
-            #elif z == len(self.allweightgradeintsarray) -1:
-                #self.allweightgradeintsarray[z] = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix)), np.transpose(self.hidden_matrix_PL_array[z-1]))
-                #self.biases_gradients_array[z] = np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix))
-            #else:
-                #self.allweightgradeintsarray[z] = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z])),np.transpose(self.hidden_matrix_PL_array[z - 1]))
-                #self.biases_gradients_array[z] = np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z]))
-
+        # Tuning is done simultaneously
         for z in range(0, len(self.allweightsarray)):
             if z == 0:
-                self.allweightsarray[z] += np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z])), np.transpose(input_matrix))
-                self.biases_array[z] += np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z]))
-            elif z == len(self.allweightgradeintsarray) -1:
-                self.allweightsarray[z] += np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix)), np.transpose(self.hidden_matrix_PL_array[z-1]))
-                self.biases_array[z] += np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix))
+                gradient_weight_temp = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ulta_relu_act(self.hidden_matrix_PL_array[z])), np.transpose(input_matrix))
+                self.allweightsarray[z] += np.clip(gradient_weight_temp, -1.0, 1.0) # Gradient clipping, (to stop those weights from getting as thicc as khalifas ass and exploding) cus the weights compound and increase
+                self.biases_array[z] += np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ulta_relu_act(self.hidden_matrix_PL_array[z]))
+            elif z == len(self.allweightsarray) -1:
+                gradient_weight_temp = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix)), np.transpose(self.hidden_matrix_PL_array[z-1]))
+                self.allweightsarray[z] += np.clip(gradient_weight_temp, -1.0, 1.0) # Gradient Clipping
+                self.biases_array[z] +=np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(output_matrix))
             else:
-                self.allweightsarray[z] += np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z])),np.transpose(self.hidden_matrix_PL_array[z - 1]))
-                self.biases_array[z] += np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ultasigmoid(self.hidden_matrix_PL_array[z]))
-
-        # tuning the weights and biases
-        #for tempid in range(0, len(self.allweightsarray)):
-            #self.allweightsarray[tempid] += self.allweightgradeintsarray[tempid]
-            #self.biases_array[tempid] += self.biases_gradients_array[tempid]
+                gradient_weight_temp = np.dot(np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ulta_relu_act(self.hidden_matrix_PL_array[z])),np.transpose(self.hidden_matrix_PL_array[z - 1]))
+                self.allweightsarray[z] += np.clip(gradient_weight_temp, -1.0, 1.0) # gradient clipping
+                self.biases_array[z] += np.multiply(np.multiply(self.learning_rate, self.allErrors[z]), self.ulta_relu_act(self.hidden_matrix_PL_array[z]))
